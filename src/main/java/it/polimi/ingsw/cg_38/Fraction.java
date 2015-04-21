912363728eebea4f073d29ce43c96e58e6114223
apiro <albypiro@me.com>
2015-04-21 17:05:51 +0200

    other new commit Signed-off-by: apiro <albypiro@me.com>

--- src/main/java/it/polimi/ingsw/cg_38/Fraction.java
+++ src/main/java/it/polimi/ingsw/cg_38/Fraction.java
@@ -4,6 +4,7 @@ public class Fraction {
 	
 	private int numerator;
     private int denominator;
+    private String myTestString;
     /**
      * Constructs a fraction with the specified numerator and denominator
      * @param numerator the numerator of the fraction
