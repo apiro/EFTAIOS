89756baa9ca71f41250a0d4b3582a026ec8a9f84
apiro <albypiro@me.com>
2015-04-21 16:37:47 +0200

    first commit

---
+++ src/main/java/it/polimi/ingsw/cg_38/App.java
@@ -0,0 +1,13 @@
+package it.polimi.ingsw.cg_38;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
