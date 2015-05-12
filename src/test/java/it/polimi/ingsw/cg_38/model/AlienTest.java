39d829ed8bcb81ccdd555e3f61dd3ceebd5d47bb
apiro <albypiro@me.com>
2015-05-12 14:14:46 +0200

    adding test case to Alien class
    Signed-off-by: apiro <albypiro@me.com>

--- src/test/java/it/polimi/ingsw/cg_38/model/AlienTest.java
+++ src/test/java/it/polimi/ingsw/cg_38/model/AlienTest.java
@@ -2,13 +2,47 @@ package it.polimi.ingsw.cg_38.model;
 
 import static org.junit.Assert.*;
 
+import java.util.Scanner;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class AlienTest {
 
+	GameModel model;
+	Sector alienStartingPoint;
+	Player player;
+	Turn actualTurn;
+	Avatar avatar;
+	Card drown; 
+	
+	@Before
+	public void init() throws InterruptedException {
+		model = new GameModel("Galilei");
+    	alienStartingPoint = model.getGameMap().searchSectorByName("AlienStartingPoint");
+    	//asp 5,11
+    	model.getGamePlayers().add(new Player("Alberto"));
+    	player = model.getGamePlayers().get(0);
+    	Thread.sleep(3000);
+    	model.getGamePlayers().get(0).setAvatar(new Alien(Name.Alien1));
+    	actualTurn = new Turn(player);
+    	model.setActualTurn(actualTurn);
+    	player.setNumTurniGiocati(player.getNumTurniGiocati());
+    	avatar = model.getGamePlayers().get(0).getAvatar();
+    	avatar.setCurrentSector(alienStartingPoint);
+    	System.out.println(avatar.getCurrentSector().toString());
+    	drown = avatar.draw(model.getDeckSector());
+	}
+	
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		Sector sector = model.getGameMap().searchSectorByCoordinates(3,9);
+		if(avatar.canMove(sector)) {
+    		avatar.move(sector, player.getNumTurniGiocati()+1);
+    		player.setNumTurniGiocati(player.getNumTurniGiocati()+1);
+    	}
+		assertEquals(avatar.canMove(sector), false);
+		assertEquals(avatar.getMyMovements().size(), 0);
 	}
-
 }
