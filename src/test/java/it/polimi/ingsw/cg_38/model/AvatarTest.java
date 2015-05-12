dbd946dbdba6b47ecfd38a6a0e626277512fe79a
apiro <albypiro@me.com>
2015-05-12 15:09:25 +0200

    update avatar test
    Signed-off-by: apiro <albypiro@me.com>

--- src/test/java/it/polimi/ingsw/cg_38/model/AvatarTest.java
+++ src/test/java/it/polimi/ingsw/cg_38/model/AvatarTest.java
@@ -2,11 +2,6 @@ package it.polimi.ingsw.cg_38.model;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
-
-import org.junit.Before;
-import org.junit.Test;
-
 public class AvatarTest {
-
+	
 }
