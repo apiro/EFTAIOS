1dd30f1cf45d96642059381ecc3d7da7495e55a5
Marco <scimmiuru@gmail.com>
2015-06-19 22:36:53 +0200

    add controllerTest
    Signed-off-by: Marco <scimmiuru@gmail.com>

---
+++ src/test/java/it/polimi/ingsw/cg_38/controller/GameControllerTest.java
@@ -0,0 +1,116 @@
+package it.polimi.ingsw.cg_38.controller;
+
+import static org.junit.Assert.*;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import it.polimi.ingsw.cg_38.controller.action.Draw;
+import it.polimi.ingsw.cg_38.controller.action.FinishTurn;
+import it.polimi.ingsw.cg_38.controller.event.NotifyEvent;
+import it.polimi.ingsw.cg_38.gameEvent.EventDraw;
+import it.polimi.ingsw.cg_38.gameEvent.EventFinishTurn;
+import it.polimi.ingsw.cg_38.model.Avatar;
+import it.polimi.ingsw.cg_38.model.Dangerous;
+import it.polimi.ingsw.cg_38.model.Fermi;
+import it.polimi.ingsw.cg_38.model.Alien;
+import it.polimi.ingsw.cg_38.model.Player;
+import it.polimi.ingsw.cg_38.notifyEvent.EventNotifyTurn;
+import it.polimi.ingsw.cg_38.notifyEvent.EventNotifyError;
+import it.polimi.ingsw.cg_38.notifyEvent.EventDrown;
+
+public class GameControllerTest {
+	
+	GameController gc;
+	String type;
+	String topic;
+	
+	Player player1;
+	Player player2;
+	Player player3;
+	
+	ArrayList<Player> add;
+	ArrayList<NotifyEvent> notifyEvents;
+	
+	FinishTurn finishTurn;
+	Draw draw;
+	EventFinishTurn evtFinishTurn;
+	EventDraw evtDraw;
+	
+	Dangerous sector1;
+	
+	int count1;
+	int count2;
+	
+	
+	
+	
+	@Before
+	public void init() throws ParserConfigurationException, Exception {
+		
+		type = "Fermi";
+		topic = "room1";
+		
+		player1 = new Player("Robben");
+		player2 = new Player("Diffi");
+		player3 = new Player("Berlu");
+		
+		sector1 = new Dangerous();
+		
+		add = new ArrayList<Player>();
+		add.add(player1);
+		add.add(player2);
+		add.add(player3);
+
+		gc = new GameController(type , topic);
+		gc.setCanAcceptOtherPlayers(true);
+		gc.getGameModel().setGamePlayers(add);
+		
+		
+	}
+
+	@Test
+	public void test() throws RemoteException {
+		
+		assertEquals(gc.getTopic() , topic);
+		assertTrue(gc.getGameModel().getGameMap() instanceof Fermi);
+		assertEquals(gc.getBuffer().size() , 0);
+		assertTrue(gc.getCanAcceptOtherPlayers());
+		gc.assignAvatars();
+		for(int i=0; i<gc.getGameModel().getGamePlayers().size(); i++){
+			gc.getGameModel().getGamePlayers().get(i).getAvatar().setCurrentSector(sector1);
+			if(gc.getGameModel().getGamePlayers().get(i).getAvatar() instanceof Alien)
+				count1++;
+			else
+				count2++;
+		}
+		assertTrue(count1 >= count2);
+		
+		gc.setFirstTurn();
+		assertEquals(gc.getGameModel().getActualTurn().getCurrentPlayer() , gc.getGameModel().getGamePlayers().get(0));
+
+		evtFinishTurn = new EventFinishTurn(gc.getGameModel().getActualTurn().getCurrentPlayer());
+		evtDraw = new EventDraw(gc.getGameModel().getActualTurn().getCurrentPlayer());
+		finishTurn = new FinishTurn(evtFinishTurn);
+		draw = new Draw(evtDraw);
+		
+		notifyEvents = gc.performUserCommands(finishTurn);
+		assertEquals(((EventNotifyTurn)notifyEvents.get(0)).getPlayerOfTurn()    , gc.getGameModel().getActualTurn().getCurrentPlayer());
+		
+		notifyEvents = gc.performUserCommands(draw);
+		assertTrue(notifyEvents.get(0) instanceof EventNotifyError);
+		gc.getGameModel().getActualTurn().setHasMoved(true);
+		gc.getGameModel().setGameState(GameState.RUNNING);
+		notifyEvents = gc.performUserCommands(draw);
+		assertTrue(notifyEvents.get(0) instanceof EventDrown);
+		
+		gc.closeGame();
+			
+	}
+
+}
